0c07e8638ff81e17b01df7c6d6d295ce176b9b1e
Anri <anri0806@hotmail.co.jp>
2021-08-06 16:19:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+let cards = [];
+
+function writeCards(name, event){
+    for (let i = 0; i < name.length; i++){
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+}
+
+
+
+
+
+function countDown(){
+    let count = 10;
+    while (count >= 0) {
+       console.log(count -= 1);
+    }
+}
